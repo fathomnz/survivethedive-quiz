9b059f89a159a65d6f9e55634f0173ab93660874
fathomed <you@example.com>
2020-12-02 20:39:19 +1300

    reduce questions to max 10

--- quiz.js
+++ quiz.js
@@ -27,7 +27,7 @@ fetch("https://fathomed.github.io/survivethedive-quiz/questions.json")
 
 // CONSTANTS
 const CORRECT_BONUS = 1;
-const MAX_QUESTIONS = 20;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
   questionCounter = 0;
